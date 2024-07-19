aa6808a9c8ea465c2847e924c1348b80a1af55c4
Simone <f.simone89@gmail.com>
2024-07-19 11:31:04 +0200

    start php project laracas

---
+++ resources/views/Components/nav-link.blade.php
@@ -0,0 +1,16 @@
+@props([
+'active' => false,
+'type' => true
+])
+
+@if($type)
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium"
+	aria-current="{{ $active ? 'page' : 'false'}}" {{$attributes}}>{{$slot}}</a>
+
+@else
+
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+	{{$slot}}
+</button>
+
+@endif
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-layout heading>
+	<x-slot:heading>
+		Home page
+	</x-slot:heading>
+	<h1>Hello home page</h1>
+
+</x-layout>
