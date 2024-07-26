9a3fd0bd1860bfc9d1a31462ec493eb01755c9e9
Simone <f.simone89@gmail.com>
2024-07-26 11:33:45 +0200

    Pivot table and belongtomany

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,5 +11,19 @@ class Job extends Model {
 
     protected $fillable = ['title', 'salary'];
 
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+        /*
+        La funzione belongsTo() serve per definire una relazione uno a molti o viceversa
+        
+        */
+
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey:"job_listing_id");
+    }
+
 
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,15 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Employer extends Model
+class Tag extends Model
 {
     use HasFactory;
 
     public function jobs()
     {
-        return $this->hasMany(Job::class);
-        /*
-        In this case we use the function hasMany() beacuse the relationship is one to many
-        */
+        return $this->belongsToMany(Job::class, relatedPivotKey: "job_listing_id");
     }
 }
